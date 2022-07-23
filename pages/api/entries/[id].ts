273f7f1ebaa943807987dcda7417c8bbcea218bc
Bruno Malagoli <bmalagoli2@gmail.com>
2022-07-23 13:59:44 -0300

    Actualización version next 12.2.3 y implementacion de endpoint para recibir entrada por ID

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -16,10 +16,29 @@ export default function handler(
   switch (req.method) {
     case "PUT":
       return updateEntry(req, res);
+    case "GET":
+      return getEntry(req, res);
     default:
       return res.status(400).json({ message: "Metodo no existente" });
   }
 }
+
+const getEntry = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { id } = req.query;
+    await db.connect();
+    const entryById = await Entry.findById(id);
+    await db.disconnect();
+    if (!entryById) {
+      res.status(400).json({
+        message: "No se encontró una entrada con el siguiente ID: " + id,
+      });
+    }
+    res.status(200).json({ message: entryById! });
+  } catch (error) {
+    res.status(400).json({ message: "Ocurrió un error" });
+  }
+};
 
 const updateEntry = async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.query;
